10a1438803ae441e564e255e6268c83d7a1d0164
Smitha Priya <smithapriya07@gmail.com>
2017-09-19 16:52:47 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -1,9 +1,18 @@
 class Foobar
-
-  def self.baz(a)
+    
+    def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
-  end
+    
+    a = a.map{|elem| elem.to_i}
+    a = a.map{ |elem| (elem+2)}
+    a.keep_if{ |elem| elem%2==0}
+    a.uniq
+    a.keep_if{|elem| elem < 10}
+    a.sum
+    #return b
+    
+end
 end
 
 
--- hw1.rb
+++ hw1.rb
@@ -1,20 +1,24 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
-
+    # Q1 CODE HERE
+    (a+b)** 2
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-
+    # Q2 CODE HERE
+    a= a.sort
+    a.map{ |elem| (elem+1) }
+    #a.sort
+    
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
-
+    # Q3 CODE HERE
+    first_name + " " + last_name
 end
 
 def blockin_time(a)
-  # DO NOT EDIT THIS CODE BELOW
-  require './foobar'
-  Foobar.baz a
+    # DO NOT EDIT THIS CODE BELOW
+    require './foobar'
+    Foobar.baz a
+    
 end
